5641bf39823021727fd8941f7bb63b0df8966ed3
fvictoria <felipevictoria630@yahoo.com>
2018-12-17 11:17:56 -0500

    ttt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        Scanner kbReader = new Scanner(System.in);
+        System.out.println("Welcome to tic tac toe");
+        System.out.println("|"+"\t"+"|"+"\t"+"|"+"\t"+"|"+"\n"+""+"\n"+"|"+"\t"+"|"+"\t"+"|"+"\t"+"|"+"\t"+"\n"+"\n"+"|"+"\t"+"|"+"\t"+"|"+"\t"+"|");
+        int cols = 3;int rows = 3;
+        int [][] array = new int[cols][rows];
+
+
     }
 }
