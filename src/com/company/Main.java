52664c30b21e085d33ea533f7b4ad552b46ce4b9
fvictoria <felipevictoria630@yahoo.com>
2018-12-17 11:20:33 -0500

    ttt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,13 @@ public class Main {
         System.out.println("|"+"\t"+"|"+"\t"+"|"+"\t"+"|"+"\n"+""+"\n"+"|"+"\t"+"|"+"\t"+"|"+"\t"+"|"+"\t"+"\n"+"\n"+"|"+"\t"+"|"+"\t"+"|"+"\t"+"|");
         int cols = 3;int rows = 3;
         int [][] array = new int[cols][rows];
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                array[i][j]=0;
+
+            }
+            
+        }
 
 
     }
